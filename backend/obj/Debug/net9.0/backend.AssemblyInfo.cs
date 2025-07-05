790bfdbf265de7bb4611edd100dde7cfa5a1fb03
ice <a1143179@gmail.com>
2025-07-05 10:04:44 +1000

    change the connection string

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a639d6711e1f6e08aca5e3dd4039efd46ef21213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c492b5a504b0d1cdf114d517541290f0d991c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
