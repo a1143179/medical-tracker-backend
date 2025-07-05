42617b684d5dfc7dc8d1c4d4e70c02c28093ba23
ice <a1143179@gmail.com>
2025-07-05 14:24:45 +1000

    fix the routing

---
+++ backend/CreateBloodSugarRecordDto.cs
@@ -0,0 +1,8 @@
+
+public class CreateBloodSugarRecordDto
+{
+    
+    public DateTime MeasurementTime { get; set; }
+    public double Level { get; set; }
+    public string? Notes { get; set; }
+}
