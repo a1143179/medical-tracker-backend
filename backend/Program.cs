ef9f36638ae08aca4fa0ad34afc9f3ad1c1efa0f
ice <a1143179@gmail.com>
2025-07-12 17:56:06 +1000

    feat: backend proxies all non-API requests to frontend dev server on port 3001 in development

--- backend/Program.cs
+++ backend/Program.cs
@@ -44,4 +44,32 @@ app.UseSession();
 app.UseAuthorization();
 app.MapControllers();
 
+// Proxy all non-API requests to frontend dev server on port 3001
+if (app.Environment.IsDevelopment())
+{
+    app.Use(async (context, next) =>
+    {
+        if (context.Request.Path.StartsWithSegments("/api"))
+        {
+            await next();
+            return;
+        }
+        var client = new HttpClient();
+        var frontendUrl = "http://localhost:3001" + context.Request.Path + context.Request.QueryString;
+        var frontendResponse = await client.GetAsync(frontendUrl);
+        context.Response.StatusCode = (int)frontendResponse.StatusCode;
+        foreach (var header in frontendResponse.Headers)
+        {
+            context.Response.Headers[header.Key] = header.Value.ToArray();
+        }
+        foreach (var header in frontendResponse.Content.Headers)
+        {
+            context.Response.Headers[header.Key] = header.Value.ToArray();
+        }
+        context.Response.Headers.Remove("transfer-encoding");
+        var content = await frontendResponse.Content.ReadAsByteArrayAsync();
+        await context.Response.Body.WriteAsync(content, 0, content.Length);
+    });
+}
+
 app.Run();
